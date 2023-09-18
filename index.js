d7317ba65736a35bbe97fcec4c5b9c60514e4f29
yuekcc <yuekcc@qq.com>
2023-09-19 00:21:21 +0800

    refactor

--- index.js
+++ index.js
@@ -1,64 +1,88 @@
 import van from 'vanjs-core';
-import HanziDb from './db';
-import { makeRandomNumberGenerator, speak } from './utils';
+import { speak } from './utils';
 import debounce from 'lodash.debounce';
-
-import './sql.js';
+import shuffe from 'lodash.shuffle';
+import * as Db from './sql';
 
 const { div } = van.tags;
 
-const hanziCount = HanziDb.length;
+async function queryHanziPosList() {
+  const result = await Db.queryBySql(`select pos from hanzi`);
+  return shuffe(result.results[0].values);
+}
 
-const generateRandomNumber = makeRandomNumberGenerator(hanziCount);
+async function queryHanziByPos(pos) {
+  const result = await Db.queryBySql(`select * from hanzi where pos = $pos`, { $pos: pos });
+  const data = {};
+  const row = result.results[0].values || [];
+  result.results?.[0].columns.forEach((col, index) => {
+    if (col === 'explanations' || col === 'pinyins') {
+      data[col] = JSON.parse(row[index] ?? '[]');
+      return;
+    }
 
-const randomIndex = van.state(generateRandomNumber());
-const displayHanzi = van.derive(() => {
-  const index = randomIndex.val;
-  return HanziDb[index];
-});
-
-const onRead = debounce(() => {
-  const hanzi = displayHanzi.val.hanzi;
-  speak(hanzi);
-}, 200);
-
-function onLookup() {}
-
-const dotNumbers = '❶❷❸❹❺❻❼❽❾❿⓫⓬⓭⓮⓯⓰⓱⓲⓳⓴'.split('');
-function ExplanationList(list) {
-  let x_ = list.join('\n');
-  dotNumbers.forEach(it => {
-    x_ = x_.replaceAll(it, `\n${it} `);
+    data[col] = row[index] ?? null;
   });
 
-  return x_.split('\n').map(it => div(it));
+  return data;
 }
 
-function TopBar({ onRead, onLookup }) {
-  return div(
-    { class: 'top-bar' },
-    div({}, div({ class: 'btn', onclick: onLookup }, '查')),
-    div({ class: 'title' }, '一级汉字'),
-    div({}, div({ class: 'btn', onclick: onRead }, '读')),
-  );
-}
+Db.init().then(async () => {
+  const posList = await queryHanziPosList();
 
-function NextHanzi() {
-  const onclick = () => {
-    randomIndex.val = generateRandomNumber();
+  const randomIndex = van.state(0);
+  const displayHanzi = van.state({});
+  van.derive(async () => {
+    const index = posList[randomIndex.val];
+    const hanzi = await queryHanziByPos(index);
+
+    displayHanzi.val = hanzi;
+  });
+
+  const onRead = debounce(() => {
+    const hanzi = displayHanzi.val.hanzi;
+    speak(hanzi);
+  }, 200);
+
+  function onLookup() {}
+
+  const dotNumbers = '❶❷❸❹❺❻❼❽❾❿⓫⓬⓭⓮⓯⓰⓱⓲⓳⓴'.split('');
+  function ExplanationList(list) {
+    let x_ = list.join('\n');
+    dotNumbers.forEach(it => {
+      x_ = x_.replaceAll(it, `\n${it} `);
+    });
+
+    return x_.split('\n').map(it => div(it));
+  }
+
+  function TopBar({ onRead, onLookup }) {
+    return div(
+      { class: 'top-bar' },
+      div({}, div({ class: 'btn', onclick: onLookup }, '查')),
+      div({ class: 'title' }, '一级汉字'),
+      div({}, div({ class: 'btn', onclick: onRead }, '读')),
+    );
+  }
+
+  function NextHanzi() {
+    const onclick = () => {
+      randomIndex.val += 1;
+    };
+
+    return div({ class: 'next-button', onclick }, '下一个');
+  }
+
+  const App = () => {
+    return div(
+      { class: 'hanzi-container', style: `height: 100vh; max-height: 100vh` },
+
+      TopBar({ onRead, onLookup }),
+      div({ class: 'hanzi' }, () => displayHanzi.val.hanzi),
+      () => div({ class: 'explanation' }, ExplanationList(displayHanzi.val.explanations)),
+      NextHanzi(),
+    );
   };
 
-  return div({ class: 'next-button', onclick }, '下一个');
-}
-
-const App = () => {
-  return div(
-    { class: 'hanzi-container', style: `height: 100vh; max-height: 100vh` },
-
-    TopBar({ onRead, onLookup }),
-    div({ class: 'hanzi' }, () => displayHanzi.val.hanzi),
-    () => div({ class: 'explanation' }, ExplanationList(displayHanzi.val.explanations)),
-    NextHanzi(),
-  );
-};
-van.add(document.body, App());
+  van.add(document.body, App());
+});
--- sql.js
+++ sql.js
@@ -1,23 +1,75 @@
 const worker = new Worker('/sql/worker.sql-wasm.js');
-worker.onmessage = evt => {
-  console.log('sqlite ready', evt.data);
+
+const nextId = (function () {
+  let x = 0;
+  return () => {
+    x += 1;
+    return x;
+  };
+})();
+
+const callbackMap = new Map();
+const dbState = {
+  ready: false,
+  callbacksBeforeReady: [],
 };
 
-fetch('/hanzi.db')
-  .then(res => res.arrayBuffer())
-  .then(buf => {
+worker.onmessage = evt => {
+  const { id, ...data } = evt.data;
+  const callback = callbackMap.get(id);
+  callback(data);
+
+  callbackMap.delete(id);
+};
+
+function openDatabase(buf) {
+  return new Promise(resolve => {
+    const id = nextId();
+    callbackMap.set(id, data => resolve(data));
+
     worker.postMessage({
-      id: 1,
+      id,
       action: 'open',
       buffer: buf,
     });
-  })
-  .then(() => {
-    setTimeout(() => {
-      worker.postMessage({
-        id: 2,
-        action: 'exec',
-        sql: `select * from sqlite_master where type = 'table'`,
-      });
-    }, 100);
   });
+}
+
+/**
+ * 通过 sql 查询数据
+ * @param {string} sql
+ * @param {object | null} params
+ * @returns {{results: Array<{values: any[], columns: string[]}>}}
+ */
+export function queryBySql(sql, params = null) {
+  return new Promise(resolve => {
+    const fn = () => {
+      const id = nextId();
+      callbackMap.set(id, data => resolve(data));
+
+      worker.postMessage({
+        id,
+        action: 'exec',
+        sql,
+        params: params || {},
+      });
+    };
+
+    if (dbState.ready) {
+      fn();
+    } else {
+      dbState.callbacksBeforeReady.push(fn);
+    }
+  });
+}
+
+export async function init() {
+  const buf = await fetch('/hanzi.db').then(res => res.arrayBuffer());
+
+  await openDatabase(buf);
+  dbState.ready = true;
+
+  if (dbState.callbacksBeforeReady.length > 0) {
+    dbState.callbacksBeforeReady.forEach(cb => cb());
+  }
+}
